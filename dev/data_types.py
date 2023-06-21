061656030255f76238505cb462083b68f09df505
jianshanglaoge <294804290@qq.com>
2023-06-21 10:42:54 -0400

    updated dev files

--- dev/data_types.py
+++ dev/data_types.py
@@ -19,6 +19,39 @@ class Person:
         self.ankle_r = Joint()
         self.pelvis = Joint()
 
+        # H5 POS Columns
+        self.H5_HEAD_LOC_X = 66
+        self.H5_HEAD_LOC_Y = 67
+        self.H5_CHEST_LOC_X = 69
+        self.H5_CHEST_LOC_Y = 70
+        self.H5_SHOULDER_L_LOC_X = 72
+        self.H5_SHOULDER_L_LOC_Y = 73
+        self.H5_ELBOW_L_LOC_X = 78
+        self.H5_ELBOW_L_LOC_Y = 79
+        self.H5_WRIST_L_LOC_X = 84
+        self.H5_WRIST_L_LOC_Y = 85
+        self.H5_HIP_L_LOC_X = 93
+        self.H5_HIP_L_LOC_Y = 94
+        self.H5_ANKLE_L_LOC_X = 99
+        self.H5_ANKLE_L_LOC_Y = 100
+        self.H5_SHOULDER_R_LOC_X = 75
+        self.H5_SHOULDER_R_LOC_Y = 76
+        self.H5_ELBOW_R_LOC_X = 81
+        self.H5_ELBOW_R_LOC_Y = 82
+        self.H5_WRIST_R_LOC_X = 87
+        self.H5_WRIST_R_LOC_Y = 88
+        self.H5_HIP_R_LOC_X = 96
+        self.H5_HIP_R_LOC_Y = 97
+        self.H5_ANKLE_R_LOC_X = 102
+        self.H5_ANKLE_R_LOC_Y = 103
+        self.H5_PELVIS_LOC_X = 90
+        self.H5_PELVIS_LOC_Y = 91
+
+        # Output Camera 2 has knees which are not needed
+        self.H5_ANKLE_L_2_LOC_X = 105
+        self.H5_ANKLE_L_2_LOC_Y = 106
+        self.H5_ANKLE_R_2_LOC_X = 108
+        self.H5_ANKLE_R_2_LOC_Y = 109
 class Arm:
 
     def __init__(self):
@@ -31,5 +64,24 @@ class Arm:
         self.finger2 = Joint()
 
         # H5 POS Columns
-        self.H5_BASE_LOC = 45
+        self.H5_BASE_LOC_X = 45
+        self.H5_BASE_LOC_Y = 46
+        self.H5_JOINT1_LOC_X = 48
+        self.H5_JOINT1_LOC_Y = 49
+        self.H5_JOINT2_LOC_X = 51
+        self.H5_JOINT2_LOC_Y = 52
+        self.H5_JOINT3_LOC_X = 54
+        self.H5_JOINT3_LOC_Y = 55
+        self.H5_JOINT4_LOC_X = 57
+        self.H5_JOINT4_LOC_Y = 58
+        self.H5_FINGER1_LOC_X = 60
+        self.H5_FINGER1_LOC_Y = 61
+        self.H5_FINGER2_LOC_X = 63
+        self.H5_FINGER2_LOC_Y = 64
 
+class Frame:
+    def __init__(self):
+        self.arm = Arm()
+        self.person = Person()
+        self.nextFrame = None
+        self.prevFrame = None
--- dev/main.py
+++ dev/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import h5py
+import data_types as dt
 
 def read_pickles(file_prefix):
     dict_assemblies = pd.read_pickle('pickle_data/'+ file_prefix +'_assemblies.pickle')
@@ -22,11 +23,90 @@ def read_h5(file_prefix):
         # If a_group_key is a group name,
         # this gets the object names in the group and returns as a list
         dataset = f[a_group_key]['table']['values_block_0']
+        indeces = list(f[a_group_key]['table']['index'])
+        frames = []
+        for index in indeces:
+            data = dataset[index]
+            frame = dt.Frame()
 
+            frame.arm.base.x = data[frame.arm.H5_BASE_LOC_X]
+            frame.arm.base.y = data[frame.arm.H5_BASE_LOC_Y]
+
+            frame.arm.joint1.x = data[frame.arm.H5_JOINT1_LOC_X]
+            frame.arm.joint1.y = data[frame.arm.H5_JOINT1_LOC_Y]
+
+            frame.arm.joint2.x = data[frame.arm.H5_JOINT2_LOC_X]
+            frame.arm.joint2.y = data[frame.arm.H5_JOINT2_LOC_Y]
+
+            frame.arm.joint3.x = data[frame.arm.H5_JOINT3_LOC_X]
+            frame.arm.joint3.y = data[frame.arm.H5_JOINT3_LOC_Y]
+
+            frame.arm.joint4.x = data[frame.arm.H5_JOINT4_LOC_X]
+            frame.arm.joint4.y = data[frame.arm.H5_JOINT4_LOC_Y]
+
+            frame.arm.finger1.x = data[frame.arm.H5_FINGER1_LOC_X]
+            frame.arm.finger1.y = data[frame.arm.H5_FINGER1_LOC_Y]
+
+            frame.arm.finger2.x = data[frame.arm.H5_FINGER2_LOC_X]
+            frame.arm.finger2.y = data[frame.arm.H5_FINGER2_LOC_Y]
+
+            frame.person.head.x = data[frame.person.H5_HEAD_LOC_X]
+            frame.person.head.y = data[frame.person.H5_HEAD_LOC_Y]
+
+            frame.person.chest.x = data[frame.person.H5_CHEST_LOC_X]
+            frame.person.chest.y = data[frame.person.H5_CHEST_LOC_Y]
+
+            frame.person.shoulder_l.x = data[frame.person.H5_SHOULDER_L_LOC_X]
+            frame.person.shoulder_l.y = data[frame.person.H5_SHOULDER_L_LOC_Y]
+
+            frame.person.shoulder_r.x = data[frame.person.H5_SHOULDER_R_LOC_X]
+            frame.person.shoulder_r.y = data[frame.person.H5_SHOULDER_R_LOC_Y]
+
+            frame.person.elbow_l.x = data[frame.person.H5_ELBOW_L_LOC_X]
+            frame.person.elbow_l.y = data[frame.person.H5_ELBOW_L_LOC_Y]
+
+            frame.person.elbow_r.x = data[frame.person.H5_ELBOW_R_LOC_X]
+            frame.person.elbow_r.y = data[frame.person.H5_ELBOW_R_LOC_Y]
+
+            frame.person.wrist_l.x = data[frame.person.H5_WRIST_L_LOC_X]
+            frame.person.wrist_l.y = data[frame.person.H5_WRIST_L_LOC_Y]
+
+            frame.person.wrist_r.x = data[frame.person.H5_WRIST_R_LOC_X]
+            frame.person.wrist_r.y = data[frame.person.H5_WRIST_R_LOC_Y]
+
+            frame.person.pelvis.x = data[frame.person.H5_PELVIS_LOC_X]
+            frame.person.pelvis.y = data[frame.person.H5_PELVIS_LOC_Y]
+
+            frame.person.hip_l.x = data[frame.person.H5_HIP_L_LOC_X]
+            frame.person.hip_l.y = data[frame.person.H5_HIP_L_LOC_Y]
+
+            frame.person.hip_r.x = data[frame.person.H5_HIP_R_LOC_X]
+            frame.person.hip_r.y = data[frame.person.H5_HIP_R_LOC_Y]
+
+            if 'cam2' in file_prefix:
+                frame.person.ankle_l.x = data[frame.person.H5_ANKLE_L_2_LOC_X]
+                frame.person.ankle_l.y = data[frame.person.H5_ANKLE_L_2_LOC_Y]
+
+                frame.person.ankle_r.x = data[frame.person.H5_ANKLE_R_2_LOC_X]
+                frame.person.ankle_r.y = data[frame.person.H5_ANKLE_R_2_LOC_Y]
+            else:
+                frame.person.ankle_l.x = data[frame.person.H5_ANKLE_L_LOC_X]
+                frame.person.ankle_l.y = data[frame.person.H5_ANKLE_L_LOC_Y]
+
+                frame.person.ankle_r.x = data[frame.person.H5_ANKLE_R_LOC_X]
+                frame.person.ankle_r.y = data[frame.person.H5_ANKLE_R_LOC_Y]
+
+            if index > 0:
+                prevFrame = frames[index-1]
+                frame.prevFrame = prevFrame
+                prevFrame.nextFrame = frame
+
+            frames.append(frame)
         pass
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    file_prefix = 'output_cam2DLC_dlcrnetms5_uf_cobotsJun7shuffle1_100000'
+    file_prefix = 'output_cam1DLC_resnet50_uf_cobots_multicamJun12shuffle1_50000'
+
     #read_pickles(file_prefix)
     read_h5(file_prefix)
     pass
